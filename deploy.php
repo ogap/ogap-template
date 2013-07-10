11fad9a096c5d9ef78bd3594e95f826fcba248fd
www-data <www-data@ogap.tk>
2013-07-10 10:30:32 +0530

    added deploy file

---
+++ deploy.php
@@ -0,0 +1,56 @@
+<?php
+	/**
+	 * GIT DEPLOYMENT SCRIPT
+	 *
+	 * Used for automatically deploying websites via github or bitbucket, more deets here:
+	 *
+	 *		https://gist.github.com/1809044
+	 */
+
+	// The commands
+
+	define('__ROOT__',dirname(__FILE__));
+		
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git pull',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+	);
+	set_include_path(get_include_path() . PATH_SEPARATOR . 'phpseclib');
+	include('Net/SSH2.php');
+	
+
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+
+	// Make it pretty for manual user access (and why not?)
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+ .  ____  .    ____________________________
+ |/      \|   |                            |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+ |___==___|  /              &copy; oodavid 2012 |
+              |____________________________|
+
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
